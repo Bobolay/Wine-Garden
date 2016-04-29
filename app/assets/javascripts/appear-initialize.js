cccacc76c3277a79acc640f44dafb72a8fb09141
Bobolay <bogdan4uk@gmail.com>
2016-04-29 17:57:02 +0300

    mobile

--- app/assets/javascripts/appear-initialize.js
+++ app/assets/javascripts/appear-initialize.js
@@ -1,7 +1,11 @@
-$(function() {
-  var $appeared = $('.dish-frame, .inner-text-right, .line-left');
-  $appeared.appear();
-  $(document.body).on('appear', function(e, $affected) {
-    $affected.addClass('appeared')
-  });
-});
+if(is_touch_screen || width < 640) {
+    console.log("it works")
+} else {
+	$(function() {
+	  var $appeared = $('.dish-frame, .inner-text-right, .line-left');
+	  $appeared.appear();
+	  $(document.body).on('appear', function(e, $affected) {
+	    $affected.addClass('appeared')
+	  });
+	});
+}

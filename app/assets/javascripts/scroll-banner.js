7ad6d63908edeac92ec6f858afe3bd4a440031af
Bobolay <bogdan4uk@gmail.com>
2016-04-29 17:08:42 +0300

    started making mobile

--- app/assets/javascripts/scroll-banner.js
+++ app/assets/javascripts/scroll-banner.js
@@ -1,56 +1,69 @@
-scroll_home_banner = function(top) {
-    if (top == undefined) {
-        top = $("#pagi2").offset().top
-    }
-    var $html_body = $('body, html')
-    var $body = $("body")
-    if(top < 0){
-        top = 0
-    }
+is_touch_screen = 'ontouchstart' in document.documentElement;
+width = window.innerWidth;
 
-    var scroll_top = $("body").scrollTop() || $(window).scrollTop()
+if(is_touch_screen || width < 640) {
+    console.log("it works")
+} else {
+    scroll_home_banner = function(top) {
 
-    if(scroll_top != top && !$body.data("scroll_in_progress")) {
-        $html_body.stop()
-        $body.data("scroll_in_progress", true)
+        var $html_body = $('body, html')
+        var $body = $("body")
+        if(top < 0){
+            top = 0
+        }
 
-        $body.data("scroll_top", top)
+        var scroll_top = $("body").scrollTop() || $(window).scrollTop()
+
+        if(scroll_top != top && !$body.data("scroll_in_progress")) {
+            $html_body.stop()
+            $body.data("scroll_in_progress", true)
+
+            $body.data("scroll_top", top)
 
 
 
-        $html_body.animate(
-            {scrollTop: top},
-            {
-                duration: 1000,
-                easing: "easeInCubic",
-                //specialEasing: "easeInOutElastic"
-                complete: function(){
-                    $body.data("scroll_in_progress", false)
+            $html_body.animate(
+                {scrollTop: top},
+                {
+                    duration: 1000,
+                    easing: "easeInCubic",
+                    //specialEasing: "easeInOutElastic"
+                    complete: function(){
+                        $body.data("scroll_in_progress", false)
+                    }
                 }
-            }
-        )
+            )
+        }
     }
-}
 
-$("body").on("wheel", function(e){
-
-    if(e && e.ctrlKey) {
-        return true
-    }
-    var $slider = $(".banner-container:first")
-    if($slider.length == 0){
-        return;
-    }
-    var slider_height = $slider.first().height()
-    var $next_section = $slider.next()
-    var next_section_top = $next_section.offset().top
-
-    var scroll_top = $("body").scrollTop() || $(window).scrollTop()
-    scroll_top = Math.ceil(scroll_top)
-    var diff = next_section_top - scroll_top
-
-    if( scroll_top < next_section_top  && e.originalEvent.deltaY > 0){
-        e.preventDefault()
+    $('.bottom').on("click", function(){
+        var $slider = $(".banner-container:first")
+        var slider_height = $slider.first().height()
+        var $next_section = $slider.next()
+        var next_section_top = $next_section.offset().top-50
         scroll_home_banner(next_section_top)
-    }
-})
+    })
+
+    $("body").on("wheel", function(e){
+
+        if(e && e.ctrlKey) {
+            return true
+        }
+        var $slider = $(".banner-container:first")
+        if($slider.length == 0){
+            return;
+        }
+        var slider_height = $slider.first().height()
+        var $next_section = $slider.next()
+        var next_section_top = $next_section.offset().top-50
+
+        var scroll_top = $("body").scrollTop() || $(window).scrollTop()
+        scroll_top = Math.ceil(scroll_top)
+        var diff = next_section_top - scroll_top
+
+        if( scroll_top < next_section_top  && e.originalEvent.deltaY > 0){
+            e.preventDefault()
+            scroll_home_banner(next_section_top)
+        }
+    })
+}

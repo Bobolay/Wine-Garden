b54d7e3b458d6691e7d9c2580190656009bdca14
pasha <p.korenev@voroninstudio.eu>
2016-06-27 14:08:44 +0300

    migrate

--- db/migrate/20160627094309_insert_dishes.rb
+++ db/migrate/20160627094309_insert_dishes.rb
@@ -1,24 +1,24 @@
 class InsertDishes < ActiveRecord::Migration
   def up
-    [{name: "салат &#171;цезар&#187;", label: "Хіт сезону", price: 110, published: true,
+    [{name: "салат &#171;цезар&#187;", label: "Хіт сезону", price: 110, published: 't',
       description: "Ніжна куриця, свіже листя салату та апетитний сир під класичним соусом подарують Вам ні з чим незрівнянний смак улюбленого салату.",
       weight: 215, image_source: "main/dish1.jpg"
      },
-     {name: "паста з лососем", label: "Пропозиція тижня", price: 120, published: true,
+     {name: "паста з лососем", label: "Пропозиція тижня", price: 120, published: 't',
       description: "Справжня італійська паста з ніжним смаком лосося – спробуйте незвичайне поєднання звичних продуктів та відчуйте неймовірний смак цієї дивовижної страви.",
       weight: 300, image_source: "main/dish2.jpg"
      },
-     {name: "форель", label: "Страва від шефа", price: 40, published: true,
+     {name: "форель", label: "Страва від шефа", price: 40, published: 't',
       description: "Соковита королівська рибка, яка тане в роті. Смачно, корисно і вишукано! Чудовим доповненням до страви стане біле вино.",
       weight: 100,
       image_source: "main/dish3.jpg"
      },
-     {name: "плов", label: "Краща пропозиція", price: 55, published: true,
+     {name: "плов", label: "Краща пропозиція", price: 55, published: 't',
       description: "Соковите м’ясо, розсипчастий рис, свіжа морква та неймовірні аромати спецій – ось він, ідеальний плов!",
       weight: 260,
       image_source: "main/dish4.jpg"
      },
-     { name: "гарбузовий крем-суп", label: "Страва від шефа", price: 55, published: true,
+     { name: "гарбузовий крем-суп", label: "Страва від шефа", price: 55, published: 't',
       description: "Ніжна консистенція, дивовижний аромат та м’який смак поживного супу придадуть енергії та здоров’я Вам та Вашій родині!",
       weight: 250, image_source: "main/dish5.jpg"
      }

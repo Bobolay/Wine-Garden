7d78c9036874057139a7fd0954ad2fe6485da40b
Bobolay <bogdan4uk@gmail.com>
2017-05-10 16:11:32 +0300

    meta tags and date picker changes

--- app/assets/javascripts/datepick.js
+++ app/assets/javascripts/datepick.js
@@ -1,6 +1,13 @@
 $(function() {
 	$("#datepicker").datepicker({
-        dateFormat: "dd.mm.yy",
+      dateFormat: "dd.mm.yy",
+      monthNames: [ "Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень" ],
+      dayNames: [ "Неділя", "Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота" ],
+      dayNamesMin: [ "Нд", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
+      dayNamesShort: [ "Нд", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб" ],
+      prevText: 'Попередній',
+      nextText: 'Наступний',
+      firstDay: 1,
 		onSelect: function(){
 			$input_wrap = $(this).closest(".input"),
 			$input_wrap.addClass("not-empty")
